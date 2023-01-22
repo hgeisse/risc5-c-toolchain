f9aaaa0737747a8ab877b602d5b0fff6f3e848c5
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2023-01-22 18:51:07 +0100

    add dummy time.h

---
+++ lib/include/time.h
@@ -0,0 +1,13 @@
+/*
+ * time.h -- time functions
+ */
+
+#ifndef __TIME_H
+#define __TIME_H
+
+/*********************************************
+ * NOTE: This is a not a full implementation 
+ *       of time.h, just the bare minimum
+ *********************************************/
+
+#endif //__TIME_H
