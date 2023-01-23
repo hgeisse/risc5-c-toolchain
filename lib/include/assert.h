be5662f2961c48fa6a60be8897d370414f08b6f3
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2023-01-23 14:28:08 +0100

    add assert.h

---
+++ lib/include/assert.h
@@ -0,0 +1,13 @@
+/*
+ * assert.h -- assertions
+ */
+
+#ifndef __ASSERT_H
+#define __ASSERT_H
+
+/*********************************************
+ * NOTE: This is a not a full implementation
+ *       of assert.h, just the bare minimum
+ *********************************************/
+
+#endif //__ASSERT_H
