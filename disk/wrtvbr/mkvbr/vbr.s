648e9a3897bb73644f687b41801ef4a8ee7948c0
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2023-02-06 20:46:24 +0100

    add wrtvbr tool to copy a dummy VBR to a partition of a disk image

---
+++ disk/wrtvbr/mkvbr/vbr.s
@@ -0,0 +1,49 @@
+//
+// vbr.s -- volume boot record
+//
+
+	.SET	serialOut,0xFFE01C	// serial output
+
+	// get some addresses listed in the load map
+	.GLOBAL	vbr
+	.GLOBAL	leave
+	.GLOBAL	msg
+
+	// the volume boot record should load the OS loader into memory
+	// this VBR is only a dummy which prints a message and exits
+vbr:
+	SUB	R14,R14,(3+2)*4		// create stack frame
+	STW	R15,R14,8		// save return address
+	STW	R8,R14,12		// save register variable
+	STW	R9,R14,16		// and another one
+	MOV	R8,msg			// pointer to string
+strloop:
+	LDB	R1,R8,0			// get char
+	BEQ	leave			// null - finished, leave
+	C	serialOut		// output char
+	ADD	R8,R8,1			// bump pointer
+	B	strloop			// next char
+leave:
+	LDW	R15,R14,8		// restore return address
+	LDW	R8,R14,12		// restore register variable
+	LDW	R9,R14,16		// and another one
+	ADD	R14,R14,(3+2)*4		// release stack frame
+	B	R15			// return
+
+	// say what is going on
+msg:
+	.BYTE	0x0D, 0x0A
+	.BYTE	"VBR executing..."
+	.BYTE	0x0D, 0x0A
+	.BYTE	0x0D, 0x0A
+	.BYTE	"This is only a sample VBR which does not load anything."
+	.BYTE	0x0D, 0x0A
+	.BYTE	"Please install a proper operating system on this partition."
+	.BYTE	0x0D, 0x0A
+	.BYTE	0x0D, 0x0A
+	.BYTE	"Bootstrap halted."
+	.BYTE	0x0D, 0x0A, 0
+
+	.SPACE	276			// adjust for sizeof(vbr) = 512
+
+	.BYTE	0x55,0xAA		// boot record signature
---
+++ disk/wrtvbr/wrtvbr.c
@@ -0,0 +1,127 @@
+/*
+ * wrtvbr.c -- write a VBR to a partition of a disk image
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+
+#define SECTOR_SIZE	512
+#define PARTTBL_OFF	0x1BE
+
+
+/**************************************************************/
+
+
+typedef struct {
+  unsigned char boot;			/* MSB is 'bootable' flag */
+  unsigned char firstSectorCHS[3];	/* not used */
+  unsigned char type;			/* type of partition */
+  unsigned char lastSectorCHS[3];	/* not used */
+  unsigned int start;			/* first sector LBA */
+  unsigned int size;			/* number of sectors */
+} PartEntry;
+
+
+/**************************************************************/
+
+
+unsigned char vbr[SECTOR_SIZE] = {
+  #include "vbr.dump"
+};
+
+
+/**************************************************************/
+
+
+void error(char *fmt, ...) {
+  va_list ap;
+
+  va_start(ap, fmt);
+  printf("Error: ");
+  vprintf(fmt, ap);
+  printf("\n");
+  va_end(ap);
+  exit(1);
+}
+
+
+/**************************************************************/
+
+
+int main(int argc, char *argv[]) {
+  char *diskName;
+  char *partNmbr;
+  FILE *disk;
+  unsigned char mbr[SECTOR_SIZE];
+  PartEntry *partTbl;
+  int i;
+  char *endp;
+  int partno;
+  unsigned int partStart;
+  unsigned int partSize;
+
+  /* check command line arguments */
+  if (argc != 3) {
+    printf("Usage: %s <disk image> <partition number>\n", argv[0]);
+    exit(1);
+  }
+  diskName = argv[1];
+  partNmbr = argv[2];
+  /* read MBR */
+  disk = fopen(diskName, "r+b");
+  if (disk == NULL) {
+    error("cannot open disk image '%s'", diskName);
+  }
+  if (fread(mbr, 1, SECTOR_SIZE, disk) != SECTOR_SIZE) {
+    error("cannot read MBR from disk image '%s'", diskName);
+  }
+  /* check boot signature */
+  if (mbr[0x1FE] != 0x55 || mbr[0x1FF] != 0xAA) {
+    error("MBR of disk '%s' has no boot signature", diskName);
+  }
+  /* show partitions */
+  partTbl = (PartEntry *) &mbr[PARTTBL_OFF];
+  printf("Partitions:\n");
+  printf("  #  type  b  start       size\n");
+  for (i = 0; i < 4; i++) {
+    printf("  %d  0x%02X  %c  0x%08X  0x%08X\n",
+           i,
+           partTbl[i].type,
+           partTbl[i].boot & 0x80 ? '*' : '-',
+           partTbl[i].start,
+           partTbl[i].size);
+  }
+  /* get partition number, check type and bootable flag */
+  partno = strtol(partNmbr, &endp, 10);
+  if (*endp != '\0') {
+    error("cannot read partition number");
+  }
+  if (partno < 0 || partno > 3) {
+    error("illegal partition number %d", partno);
+  }
+  if (partTbl[partno].type == 0) {
+    error("partition %d does not contain a file system", partno);
+  }
+  if ((partTbl[partno].boot & 0x80) == 0) {
+    error("partition %d is not bootable", partno);
+  }
+  /* determine start and size of partition */
+  partStart = partTbl[partno].start;
+  partSize = partTbl[partno].size;
+  if (partSize == 0) {
+    error("partition %d has no sectors", partno);
+  }
+  /* copy VBR to partition on disk image */
+  printf("Now going to copy the VBR to partition %d...\n", partno);
+  fseek(disk, partStart * SECTOR_SIZE, SEEK_SET);
+  if (fwrite(vbr, 1, SECTOR_SIZE, disk) != SECTOR_SIZE) {
+    error("cannot write VBR to disk image '%s'", diskName);
+  }
+  printf("...done\n");
+  fclose(disk);
+  return 0;
+}
